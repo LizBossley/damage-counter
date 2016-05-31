12246930143cf0f759df6a4f703f9af109d99c84
Liz <elizabeth.bossley@oyova.com>
2016-05-31 09:01:54 -0400

    Initial commit of index/bootstrap

---
+++ index.php
@@ -0,0 +1,12 @@
+<head>
+	<link rel="import" href="resources/imports/resources.html" >
+
+
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+			<p> Welcome to this home page </p>
+		</div>
+	</div>
+</body>
